b4459f3e53b892464dea8e312a0398f70a74e999
CyberRoshan <roshanchaurasia990@gmail.com>
2025-01-19 02:02:57 +0530

    add more projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -7,52 +7,134 @@ import mock07 from '../assets/images/giexpimg.png';
 import mock08 from '../assets/images/lawprepimg.png';
 import mock09 from '../assets/images/sushma-img.png';
 import mock10 from '../assets/images/frankoakimg.png';
+import bahari from '../assets/images/bahari.png';
+import edushrine from '../assets/images/edushrine.png';
+import econ from '../assets/images/econ.png';
+import econ2 from '../assets/images/econ2.png';
+import multiweb from '../assets/images/multiweb.png';
+import fashionz from '../assets/images/fashionz.png';
+import susmmajs from '../assets/images/sushma-js.png';
+import plantour from '../assets/images/plantour.png';
 import '../assets/styles/Project.scss';
 
 function Project() {
+    function openLinks() {
+        window.open("https://frankandoak-website.onrender.com", "_blank");
+        window.open("https://frank-and-oak-backend.onrender.com", "_blank");
+      }
     return(
     <div className="projects-container" id="projects">
         <h1>Personal Projects</h1>
         <div className="projects-grid">
             <div className="project">
+            <div onClick={openLinks} className="cursor-pointer">
                 <img src={mock10} className="zoom" alt="thumbnail" width="100%"/>
-                <h2>Frank and Oak Ecommerce</h2>
+                </div>
+                <h2><div className="cursor-pointer" onClick={openLinks}>Frank and Oak Ecommerce</div></h2>
                 <p>Developed an ecommerce platform with customer-facing site and admin panel for product,
                 category, and order management.</p>
             </div>
             <div className="project">
-                <img src={mock09} className="zoom" alt="thumbnail" width="100%"/>
-                <h2>Sushma Group Real State</h2>
-                <p>Developed the Sushma Group website with responsive and visually appealing design.</p>
-            </div>
-            <div className="project">
-                <img src={mock08} className="zoom" alt="thumbnail" width="100%"/>
-                <h2>LawPrepTutorial</h2>
-                <p>Developed a responsive web platform providing tutorials and resources for law students.</p>
-            </div>
-            <div className="project">
+                <a href="https://giexpresstaxi.co.uk/cabtaxi" rel="noreferrer" target="_blank">
                 <img src={mock07} className="zoom" alt="thumbnail" width="100%"/>
-               <h2>Gi Express Taxi</h2>
+                </a>
+               <h2><a href="https://giexpresstaxi.co.uk/cabtaxi" rel="noreferrer" target="_blank">Gi Express Taxi</a></h2>
                 <p>Developed fully responsive customer and driver dashboards, developing 15+
                 pages based on my design expertise and experience.</p>
             </div>
             <div className="project">
+            <a href="https://sushmagroup.realtynivesh.com" rel="noreferrer" target="_blank">
+                <img src={mock09} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+                <h2><a href="https://sushmagroup.realtynivesh.com" rel="noreferrer" target="_blank">Sushma Group Real State</a></h2>
+                <p>Developed the Sushma Group website with responsive and visually appealing design.</p>
+            </div>
+            <div className="project">
+            <a href="https://www.multiverbalcreations.com" rel="noreferrer" target="_blank">
+                <img src={multiweb} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+                <h2><a href="https://www.multiverbalcreations.com" rel="noreferrer" target="_blank">Multi Verbal Creations</a></h2>
+                <p>Developed the Sushma Group website with responsive and visually appealing design.</p>
+            </div>
+            <div className="project">
+                <a href="https://bahri-frontend.vercel.app/home.html" rel="noreferrer" target="_blank">
+                <img src={bahari} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+                <h2><a href="https://bahri-frontend.vercel.app/home.html" rel="noreferrer" target="_blank">Bahari</a></h2>
+                <p>Developed the Sushma Group website with responsive and visually appealing design.</p>
+            </div>
+            <div className="project">
+                <a href="https://www.econmarketresearch.com/" rel="noreferrer" target="_blank">
+                <img src={econ} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+                <h2><a href="https://www.econmarketresearch.com/" rel="noreferrer" target="_blank">Econ Market Researchs</a></h2>
+                <p>Developed the Sushma Group website with responsive and visually appealing design.</p>
+            </div>
+            <div className="project">
+            <a href="https://edushrine.in/" rel="noreferrer" target="_blank">
+                <img src={edushrine} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+                <h2><a href="https://edushrine.in/" rel="noreferrer" target="_blank">Edushrine</a></h2>
+                <p>Developed the Sushma Group website with responsive and visually appealing design.</p>
+            </div>
+            <div className="project">
+                <a href="https://econresearchs.com/" rel="noreferrer" target="_blank">
+                <img src={econ2} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+                <h2><a href="https://econresearchs.com/" rel="noreferrer" target="_blank">Econ Researchs</a></h2>
+                <p>Developed the Sushma Group website with responsive and visually appealing design.</p>
+            </div>
+            <div className="project">
+                <a href="https://cyberroshan.github.io/lawpreptutorial.github.io" rel="noreferrer" target="_blank">
+                <img src={mock08} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+                <h2><a href="https://cyberroshan.github.io/lawpreptutorial.github.io" rel="noreferrer" target="_blank">LawPrepTutorial</a></h2>
+                <p>Developed a responsive web platform providing tutorials and resources for law students.</p>
+            </div>
+            <div className="project">
+                <a href="https://traveinstein.com/" rel="noreferrer" target="_blank">
                 <img src={mock06} className="zoom" alt="thumbnail" width="100%"/>
-                <h2>Traveinstein: Plan Your Travel</h2>
+                </a>
+                <h2><a href="https://traveinstein.com/" rel="noreferrer" target="_blank">Traveinstein: Plan Your Travel</a></h2>
                 <p>Developed fully responsive Landing Pages, User dashboards, CMS Portal, developing 15+
                 pages.</p>
             </div>
             <div className="project">
+                <a href="https://kamma-admin-panel-rdzf.vercel.app" rel="noreferrer" target="_blank">
                 <img src={mock05} className="zoom" alt="thumbnail" width="100%"/>
-                <h2>KAMMA: Administrative Dashboard</h2>
+                </a>
+                <h2><a href="https://kamma-admin-panel-rdzf.vercel.app" rel="noreferrer" target="_blank">KAMMA: Administrative Dashboard</a></h2>
                 <p>Developed the Administrative Dashboard for KAMMA included design, responsiveness, development of
                 Dashboard in HTML5, Tailwind CSS, jQuery, JavaScript.</p>
             </div>
             <div className="project">
+                <a href="https://zenvi-landing-page.vercel.app" rel="noreferrer" target="_blank">
                 <img src={mock04} className="zoom" alt="thumbnail" width="100%"/>
-                <h2>Zenvi : Unlock the power of thinking</h2>
+                </a>
+                <h2><a href="https://zenvi-landing-page.vercel.app" rel="noreferrer" target="_blank">Zenvi : Unlock the power of thinking</a></h2>
                 <p>Built a responsive landing page with Animation on Scroll (AOS) for dynamic
                 UX, converting a desktop-only Figma design to a mobile-friendly version.</p>
+            </div>
+            <div className="project">
+                <a href="https://cyberroshan.github.io/Sushma-Service-Partner-Clone.github.io" rel="noreferrer" target="_blank">
+                <img src={susmmajs} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+                <h2><a href="https://cyberroshan.github.io/Sushma-Service-Partner-Clone.github.io" rel="noreferrer" target="_blank">Sushma Landing Page</a></h2>
+                <p>Developed a responsive website for Sushma Real State to display their Projects using HTML5, CSS3, Javascript.</p>
+            </div>
+            <div className="project">
+                <a href="https://cyberroshan.github.io/Fashionz-Website-Clone.github.io" rel="noreferrer" target="_blank">
+                <img src={fashionz} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+                <h2><a href="https://cyberroshan.github.io/Fashionz-Website-Clone.github.io" rel="noreferrer" target="_blank">Fashionz by Danube</a></h2>
+                <p>Developed a responsive website for Fashionz by Danube to display their Plots and Flats usig HTML5, CSS3, Jquery and Javascript.</p>
+            </div>
+            <div className="project">
+                <a href="https://cyberroshan.github.io/Kerala-Plan-Tours.github.io" rel="noreferrer" target="_blank">
+                <img src={plantour} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+                <h2><a href="https://cyberroshan.github.io/Kerala-Plan-Tours.github.io" rel="noreferrer" target="_blank">Plan Your Tours</a></h2>
+                <p>Developed a responsive website for Plan your tours to display their destinations packages usig HTML5, CSS3 and Javascript.</p>
             </div>
             <div className="project">
                 <img src={mock03} className="zoom" alt="thumbnail" width="100%"/>
