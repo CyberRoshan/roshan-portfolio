8e39e222d05eaf5fcc7905042348d24dd1fbd8e0
CyberRoshan <roshanchaurasia990@gmail.com>
2025-08-27 13:46:16 +0530

    added wp projects

--- src/components/WordPressProjects.tsx
+++ src/components/WordPressProjects.tsx
@@ -6,6 +6,12 @@ import tresdias from '../assets/images/4-min.png';
 import paralaps from '../assets/images/6-min.png';
 import pressurewashing from '../assets/images/8-min.png';
 import sototrim from '../assets/images/7-min.png';
+import mittul from '../assets/images/mittul.png';
+import yippezy from '../assets/images/yippezy.png';
+import housefull from '../assets/images/housefull.png';
+import accelence from '../assets/images/accelence.png';
+import urbannext from '../assets/images/urbannext.png';
+import stories from '../assets/images/stories.png';
 import travelinfoline from '../assets/images/5-min.png';
 import '../assets/styles/Project.scss';
 
@@ -14,6 +20,34 @@ export default function WordPressProjects() {
    <div className="projects-container" id="projects">
         <h1 className='tech-title'>WordPress Projects</h1>
         <div className="projects-grid">
+            <div className="project">
+                <a href="https://storiesunwired.com" rel="noreferrer" target="_blank">
+                <img src={stories} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+               <h2><a href="https://storiesunwired.com" rel="noreferrer" target="_blank">Stories Unwired - Figma to Wordpress</a></h2>
+                <p>Stories Unwired – A clean, responsive marketing agency website developed with Figma to WordPress for providing services and growth of businesses.</p>
+            </div>
+            <div className="project">
+                <a href="https://urbannxtinfra.com" rel="noreferrer" target="_blank">
+                <img src={urbannext} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+               <h2><a href="https://urbannxtinfra.com" rel="noreferrer" target="_blank">Urbannxt Infra - Investment Company</a></h2>
+                <p>Urbannxt Infra – A clean, responsive website developed in WordPress for Real Estate, Land and Industrial Investment in Gujarat.</p>
+            </div>
+            <div className="project">
+                <a href="https://accelance.io" rel="noreferrer" target="_blank">
+                <img src={accelence} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+               <h2><a href="https://accelance.io" rel="noreferrer" target="_blank">Accelance - Figma to Wordpress</a></h2>
+                <p>Accelance – A clean, responsive custom website developed with Figma to WordPress to showcase about Business and Automation services.</p>
+            </div>
+            <div className="project">
+                <a href="https://www.mittulshah.com/" rel="noreferrer" target="_blank">
+                <img src={mittul} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+               <h2><a href="https://www.mittulshah.com/" rel="noreferrer" target="_blank">Mittul Shah - Freelancer Website</a></h2>
+                <p>Mittul Shah - Freelancer – A clean, responsive website developed in WordPress to showcase about freelancer and marketing services.</p>
+            </div>
             <div className="project">
                 <a href="https://trueviewlandscaping.com" rel="noreferrer" target="_blank">
                 <img src={trueview} className="zoom" alt="thumbnail" width="100%"/>
@@ -62,6 +96,20 @@ export default function WordPressProjects() {
                 </a>
                 <h2><a href="https://sototrim.com" rel="noreferrer" target="_blank">SotoTrim Company</a></h2>
                 <p>SotoTrim – A sleek WordPress website showcasing expert automotive trimming and restyling services with a strong visual portfolio.</p>
+            </div>
+            <div className="project">
+                <a href="https://housefull.travelinfoline.us" rel="noreferrer" target="_blank">
+                <img src={housefull} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+               <h2><a href="https://housefull.travelinfoline.us" rel="noreferrer" target="_blank">Housefull Entertainment - Landing Page</a></h2>
+                <p>Housefull Entertainment – A clean, responsive landing page developed in WordPress to showcase about Event Management and services.</p>
+            </div>
+            <div className="project">
+                <a href="https://yipeezy.net" rel="noreferrer" target="_blank">
+                <img src={yippezy} className="zoom" alt="thumbnail" width="100%"/>
+                </a>
+               <h2><a href="https://yipeezy.net" rel="noreferrer" target="_blank">Yipeezy LLC - Custom Landing Page</a></h2>
+                <p>Yipeezy LLC – A clean, responsive custom landing page developed in WordPress to showcase about Business and consulting services.</p>
             </div>
             <div className="project">
                 <a href="https://travelinfoline.us" rel="noreferrer" target="_blank">
